49ddb5a9acbb0ab77744344424b08c364e664874
Parth Patel <parth_patel@comcast.com>
2020-12-30 01:31:48 -0500

    use of useNavigate

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+
 import PageNotFound from "./PageNotFound";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 
 export default function Detail() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const { data: product, error, loading } = useFetch(`products/${id}`);
 
     if (loading) return <Spinner />;
@@ -17,6 +19,9 @@ export default function Detail() {
         <h1>{ product.name }</h1>
         <p>{ product.description }</p>
         <p id="proce">${ product.price }</p>
+        <p>
+            <button className="btn btn-primary" onClick={(() => navigate("/cart"))}>Add to cart</button>
+        </p>
         <img src={`/images/${product.image}`} alt={product.category}/>
     </div>
   );
