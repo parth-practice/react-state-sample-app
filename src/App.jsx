55aa6a5c156d46a56ca77a6b2a4c59b60922c7e6
Parth Patel <parth_patel@comcast.com>
2020-12-31 17:05:28 -0500

    addint props method to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
 
 import "./App.css";
@@ -10,6 +10,23 @@ import Cart from './Cart';
 
 export default function App() {
 
+  const [cart, setCart] = useState([]);
+
+  function addToCart(id, sku) {
+    // console.log(":::", cart);
+    setCart((items) => {
+      const itemInCart = items.find((i) => i.sku === sku);
+
+      if (itemInCart) {
+        // return new array with matching item replace
+        return items.map((i) => i.sku === sku ? {...i, quantity: i.quantity + 1 } : i);
+      } else {
+        // return new array with new item added
+        return [ ...items, { id, sku, quantity: 1 } ];
+      }
+    });
+  }
+
   return (
     <>
       <div className="content">
@@ -18,7 +35,7 @@ export default function App() {
           <Routes>
             <Route path='/' element={<h1> Welcome to the Online shopping </h1>} />
             <Route path='/:category' element={<Products/>} />
-            <Route path='/:category/:id' element={<Detail />} />
+            <Route path='/:category/:id' element={<Detail addToCart={addToCart}/>} />
             <Route path='/cart' element={<Cart />}/>
           </Routes>
         </main>
--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from 'react-router-dom';
 
 import PageNotFound from "./PageNotFound";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 
-export default function Detail() {
+export default function Detail(props) {
     const { id } = useParams();
     const navigate = useNavigate();
+    const [ sku, setSku ] = useState("");
     const { data: product, error, loading } = useFetch(`products/${id}`);
 
     if (loading) return <Spinner />;
@@ -19,8 +20,17 @@ export default function Detail() {
         <h1>{ product.name }</h1>
         <p>{ product.description }</p>
         <p id="proce">${ product.price }</p>
+        <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+            <option value="">What sizes?</option>
+            {product.skus.map((sku) => (
+                <option key={sku.sku} value={sku.sku}>{sku.size}</option>                
+            ))}           
+        </select>
         <p>
-            <button className="btn btn-primary" onClick={(() => navigate("/cart"))}>Add to cart</button>
+            <button disabled={!sku} className="btn btn-primary" onClick={() => {
+                props.addToCart(id, sku);
+                navigate("/cart");
+            }}>Add to cart</button>
         </p>
         <img src={`/images/${product.image}`} alt={product.category}/>
     </div>
